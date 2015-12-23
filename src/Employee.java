e3e566a428a25551f677e94579f78e820a2464e9
Colin Hill <colin@peak2peakmedia.com>
2015-12-23 23:36:53 +0100

    Init Commit

---
+++ src/Employee.java
@@ -0,0 +1,44 @@
+/**
+ * Created by colinhill on 12/22/15.
+ */
+
+
+// STEP 1: This Class Represents 1 Database Table called EMPLOYEE
+public class Employee {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int salary;
+
+    public Employee() {}
+
+    public Employee(String fname, String lname, int salary) {
+        this.firstName = fname;
+        this.lastName = lname;
+        this.salary = salary;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId( int id ) {
+        this.id = id;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName( String first_name ) {
+        this.firstName = first_name;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName( String last_name ) {
+        this.lastName = last_name;
+    }
+    public int getSalary() {
+        return salary;
+    }
+    public void setSalary( int salary ) {
+        this.salary = salary;
+    }
+}
